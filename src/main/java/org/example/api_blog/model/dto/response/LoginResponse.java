33919530187a39c1f6e775759d3a9cc26ed64b36
chantha1212 <tha754688@gmail.com>
2026-04-29 21:03:03 +0700

    extra login

---
+++ src/main/java/org/example/api_blog/model/dto/response/LoginResponse.java
@@ -0,0 +1,14 @@
+package org.example.api_blog.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private String token;
+}
